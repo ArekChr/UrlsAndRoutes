6764b5164bd8e35adbfacc202172b57e91cf35e1
Aras <arkadiuszchr@gmail.com>
2017-09-03 14:55:37 +0200

    Route tests

--- UrlsAndRoutes.Tests/UnitTest1.cs
+++ UrlsAndRoutes.Tests/UnitTest1.cs
@@ -1,14 +1,97 @@
 ﻿using System;
+using System.Web;
+using System.Web.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Reflection;
 
 namespace UrlsAndRoutes.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RouteTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
         {
+            //tworzenie imitacji żądania
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath).Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+
+            //tworzenie imitacji odpowiedzi
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+
+            //tworzenie imitacji kontekstu z uzyciem żądania i odpowiedzi
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            //zwraca imitacje kontekstu
+            return mockContext.Object;
         }
+
+        private void TestRouteMatch(string url, string controller, string action,
+            object routeProperties = null, string httpMethod = "GET")
+        {
+            //przygotowanie 
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            //działanie - przetwarzanie trasy
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+            //asercje
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(v1, v2) == 0;
+            };
+            bool result = valCompare(routeResult.Values["controller"], controller) && valCompare(routeResult.Values["action"], action);
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name) && valCompare(routeResult.Values[pi.Name], 
+                        pi.GetValue(propertySet, null))))
+                    {
+                        result = false; break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void TestRouteFail(string url)
+        {  
+            // przygotowanie 
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // działanie — przetwarzanie trasy 
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+
+            // asercje  
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+
+        [TestMethod]
+        public void TestIncomingRoutes()
+        {
+            // sprawdzenie, czy otrzymamy adres URL, jakiego oczekiwaliśmy  
+            TestRouteMatch("~/Admin/Index", "Admin", "Index");
+
+            // sprawdzenie wartości uzyskanych z segmentów   
+            TestRouteMatch("~/One/Two", "One", "Two");
+
+            // upewnienie się, że za mało lub za dużo segmentów spowoduje błąd dopasowania  
+            TestRouteFail("~/Admin/Index/Segment");
+            TestRouteFail("~/Admin");
+        } 
     }
 }
--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,13 +11,10 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute("MyRoute", "{controller}/{action}", new { action = "Index" });
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            //routes.Add("MyRoute", myRoute);
         }
     }
 }
